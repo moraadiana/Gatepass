b2852c5d7201f5ece6390335cf5131bdbcdbc4ff
Manasse Gitau <manasse.gitau@bulkstream.co>
2024-02-02 13:01:05 +0300

    Add Email Queuing

--- app/Mail/GatepassRejected.php
+++ app/Mail/GatepassRejected.php
@@ -9,7 +9,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class GatepassRejected extends Mailable
+class GatepassRejected extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
