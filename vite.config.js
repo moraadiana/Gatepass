c5a61a814223c43123dd0adf711bc81ab1d0f2f8
moraadiana <moraadiana13@gmail.com>
2024-03-08 10:00:30 +0300

    done

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import laravel from "laravel-vite-plugin";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+    build: {
+        chunkSizeWarningLimit: 4000, // Adjust this value based on your project's requirements
+      },
     plugins: [
         laravel({
             input: "resources/js/app.jsx",
